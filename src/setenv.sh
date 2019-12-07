af936c2da5edd097893a7bba3e68af2f4e7aea89
郭子钦 <37041136+qq814504797@users.noreply.github.com>
2019-12-07 15:11:19 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
 export GITACCOUNT=qq814504797
 
 export MYCLUSTER=tektonknativebeijing23
-export KUBECONFIG=
+export KUBECONFIG=/usr/shared-data/cloud-ibm-com-47b84451ab70b94737518f7640a9ee42-1/.bluemix/plugins/container-service/clusters/kubeconsh-guoyc/kube-config-syd01-kubeconsh-guoyc.yml
 
-export NAMESPACE=
+export NAMESPACE=tektonknativebeijing23
 export EMAIL=gzq2132@163.com
-export INGRESS=
+export INGRESS=knative-guoyc.jp-tok.containers.appdomain.cloud
